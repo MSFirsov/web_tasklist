ba6b67aae315318647e929184e042d8752489d8a
MSFirsov <mxfrsv@gmail.com>
2023-07-10 16:25:40 +0300

    создание столбцов ежедневника через цикл

--- webapp/task/forms.py
+++ webapp/task/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class CreateTaskForm(FlaskForm):
     num_week_day = HiddenField('num_week_day', validators=[DataRequired()])
-    task_text = StringField('Текст задание', validators=[DataRequired()],
-                            render_kw={'class': 'form-control', 'placeholder': 'Input task'})
+    task_text = StringField(validators=[DataRequired()],
+                            render_kw={'class': 'form-control', 'placeholder': 'Введите текс задания'})
     submit = SubmitField('Создать!', render_kw={'class': 'btn btn-outline-secondary'})
